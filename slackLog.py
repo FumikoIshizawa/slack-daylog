bc8f0bf6cc452c3aab7afc354281a5a09356005b
FumikoIshizawa <p.krrr.731@gmail.com>
2015-10-08 02:17:19 +0900

    ログ数に応じてスコアを取得

--- slackLog.py
+++ slackLog.py
@@ -3,6 +3,7 @@
 import os
 from datetime import datetime, timedelta
 from slack import Slack
+from progress import Progress
 
 api_token = os.environ['SLACK_API_TOKEN']
 now = datetime.now()
@@ -10,11 +11,13 @@ oldest_day=-1
 duration_day=1
 oldest = datetime(now.year, now.month, now.day, 0, 0, 0, 0) + timedelta(days=oldest_day)
 latest = oldest + timedelta(days=duration_day)
+score_all = 0
 
 print(oldest, ' ~ ', latest)
 
 # get all id
 log = Slack(api_token, oldest, latest)
+progress = Progress()
 ids = log.get_channel_list()
 
 # get history(logs) 
@@ -22,4 +25,9 @@ for id in ids:
 	info = log.get_channel_info(id)
 	history = log.get_history(id)
 	print('#', info['name'], '(', len(info['members']), 'members) -', len(history), 'logs')
-	print(history, '\n')
+	print(history)
+	score = progress.score_log(len(history))
+	print('score:', score, '\n')
+	score_all += score
+
+print('Score: ', score_all)
