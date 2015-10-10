7881f7bbcee83d8faa16cb05b78ad3e06ab6d111
FumikoIshizawa <p.krrr.731@gmail.com>
2015-10-10 15:29:32 +0900

    Slackクラスを改良し、スコア計算のプロトタイプを作成

--- slackLog.py
+++ slackLog.py
@@ -1,14 +1,11 @@
 # Python3
 
-import os
 from datetime import datetime, timedelta
 from mecabParse import MecabParse
 from slack import Slack
-from progress import Progress
 
-api_token = os.environ['SLACK_API_TOKEN']
 now = datetime.now()
-oldest_day=-1
+oldest_day=-3
 duration_day=1
 oldest = datetime(now.year, now.month, now.day, 0, 0, 0, 0) + timedelta(days=oldest_day)
 latest = oldest + timedelta(days=duration_day)
@@ -17,21 +14,20 @@ score_all = 0
 print(oldest, ' ~ ', latest)
 
 # get all id
-log = Slack(api_token, oldest, latest)
-progress = Progress()
-ids = log.get_channel_list()
+ids = Slack.get_channel_list()
 
 # get history(logs) 
 for id in ids:
-	info = log.get_channel_info(id)
-	history = log.get_history(id)
-	print('#', info['name'], '(', len(info['members']), 'members) -', len(history), 'logs')
-	print(history)
-	score = progress.score_log(len(history))
+	log = Slack(oldest, latest, id)
+	print('#', log.info['name'], '(', len(log.info['members']), 'members) -', len(log.logs), 'logs')
+	print(log.logs)
+
+	score = log.score_log()
 	print('score:', score, '\n')
 	score_all += score
+
 	p = MecabParse()
-	for message in history:
+	for message in log.logs:
 		p.parse(message)
 	print(p.get_params())
 
